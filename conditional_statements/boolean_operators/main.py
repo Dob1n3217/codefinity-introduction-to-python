e155a1726c9c21b45aeaf405b99fcd2f32b98eb5
Dob1n3217 <>
2026-04-15 19:50:14 +0000

    Boolean Operators

---
+++ conditional_statements/boolean_operators/main.py
@@ -0,0 +1,13 @@
+# ==  (equal)
+# != (not equal)
+
+# > (greater than)
+# < (less than)
+
+# Combination:
+# >= (greater than or equal to) (and <=)
+
+total_cost = 25.00
+dscount_threshold = 20.00
+discountEligible = total_cost >= dscount_threshold
+print("Is the purchase eligible for a discount?", discountEligible)
